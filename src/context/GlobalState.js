d05cac4f2b9b2bb7f1f9cd3912312bb986db64a8
awaisajmal12 <76112390+awaisajmal12@users.noreply.github.com>
2020-12-22 15:55:27 +0500

    EXPENSE_TRACKER_APP

---
+++ src/context/GlobalState.js
@@ -0,0 +1,38 @@
+// eslint-disable-next-line
+import React, { createContext, useReducer } from 'react';
+import AppReducer from './AppReducer.js';
+//Initial state
+const initialState = {
+
+    transactions: []
+    
+}
+
+export const GlobalContext = createContext(initialState);
+
+export const GlobalProvider = ({ children }) => {
+
+const [state, dispatch] = useReducer(AppReducer, initialState);
+function deleteTransaction(id) { 
+    dispatch({
+        type: 'DELETE_TRANSACTION',
+        payload: id
+    });
+}
+  function addTransaction(transaction) { 
+    dispatch({
+        type: 'ADD_TRANSACTION',
+        payload: transaction
+    });
+}
+return(
+    <GlobalContext.Provider value={{ 
+        transactions: state.transactions,
+    deleteTransaction,
+    addTransaction
+    }}>
+        { children }
+    </GlobalContext.Provider>
+);
+
+}
